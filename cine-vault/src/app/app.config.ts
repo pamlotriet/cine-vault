08f5c02c07bea2da761e91109fa70e077f903607
Pamela <plotriet@retrorabbit.co.za>
2025-01-02 14:39:19 +0200

    init commit

--- cine-vault/src/app/app.config.ts
+++ cine-vault/src/app/app.config.ts
@@ -1,6 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -11,6 +13,9 @@ export const appConfig: ApplicationConfig = {
     providePrimeNG({
       theme: {
         preset: Aura,
+        options: {
+          darkModeSelector: false || 'none',
+        },
       },
     }),
   ],
